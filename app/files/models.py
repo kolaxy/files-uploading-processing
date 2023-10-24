e55fb2db9c9cace125d1f7780b952821505a40dd
kolaxy <vybornoff@outlook.com>
2023-10-25 00:09:30 +0300

    drf draft structure

--- app/files/models.py
+++ app/files/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    file = models.FileField()
+    uploaded_at = models.DateTimeField(auto_now_add=True, blank=True)
+    processed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = ("File")
+        verbose_name_plural = ("Files")
--- app/files/views.py
+++ app/files/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView
+from files.serializers import FileSerializer
+from files.models import File
 # Create your views here.
+
+class FileCreateAPIView(ListCreateAPIView):
+    serializer_class = FileSerializer
+
+    def get_queryset(self):
+        return File.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
