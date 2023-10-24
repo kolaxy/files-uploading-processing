6ed20ed11f07763ef803d5f8a07760c0a5543383
kolaxy <vybornoff@outlook.com>
2023-10-25 02:05:00 +0300

    celery task logging added

--- app/files/tasks.py
+++ app/files/tasks.py
@@ -1,8 +1,19 @@
 from celery import shared_task
 from .models import File
 
+import logging
+logger = logging.getLogger(__name__)
+
 @shared_task
-def file_processing(fileid):
-    file = File.objects.get(pk=fileid)
-    file.processed = True
-    file.save()
+def file_processing(fileid) -> None:
+    """
+    Celery task.
+    Change "processed" boolean field to True.
+    Call with transaction.on_commit due successful DB transaction.
+    """
+    try:
+        file = File.objects.get(pk=fileid)
+        file.processed = True
+        file.save()
+    except Exception as e:
+        logger.error(f"Error processing file (File ID {fileid}): {str(e)}")
